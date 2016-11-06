562752daf79d686d7a9808d1ea186bfd320ffbc9
gyhggg <gyhggg@gyhgggdeMacBook-Air.local>
2016-11-06 17:27:07 +0800

    创建工程

---
+++ wangyiNews/wangyiNews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wangyiNews
+//
+//  Created by gyhggg on 16/11/6.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
